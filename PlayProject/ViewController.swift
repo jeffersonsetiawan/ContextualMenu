8500ccbb93f1adbb7b69dfe972ebcf125f7e80bb
Jefferson Setiawan <jefferson.setiawan@icloud.com>
2020-02-12 17:23:26 +0700

    Initial Commit

---
+++ PlayProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayProject
+//
+//  Created by Jefferson Setiawan on 12/02/20.
+//  Copyright © 2020 Jefferson Setiawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
